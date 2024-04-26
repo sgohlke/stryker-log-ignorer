57e87251c19c57c725666c435f2a6e45b161072b
Hero Wanders <hero.wanders@arcor.de>
2024-04-27 00:28:18 +0200

    use babel to create NodePath objects in tests instead of faking them

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,27 +1,67 @@
-import { LogIgnorer, NodePath } from '@/index'
-import { expect, test } from 'vitest'
-// import {  testInjector } from '@stryker-mutator/test-helpers';
+import { LogIgnorer, LogIgnorerOptions } from '@/index'
 
-const pathMock: NodePath = {
-    isExpressionStatement: () => true,
-    node: {
-        expression: {
-            type: 'CallExpression',
-            callee: {
-                type: 'MemberExpression',
-                object: {
-                    type: 'Identifier',
-                    name: 'console',
-                },
-            },
+import parser from '@babel/parser'
+import traverse from '@babel/traverse'
+import { NodePath } from '@stryker-mutator/api/ignore'
+import { describe, expect, test } from 'vitest'
+
+describe('LogIgnorer.shouldIgnore', () => {
+    test.each(['log', 'debug', 'info', 'warn', 'error'])(
+        'returns a truthy value when calling console.%s',
+        (method) => {
+            const logIgnorer = new LogIgnorer()
+            const path = parseExpressionStatement(`console.${method}("test")`)
+            expect(logIgnorer.shouldIgnore(path)).toBeTruthy()
         },
-    },
-}
-
-test('Test LogIgnorer function', () => {
-    // Call each log function once. Should call console.log for all log levels
-    const logIgnorer = new LogIgnorer()
-    expect(logIgnorer.shouldIgnore(pathMock)).toBe(
-        'We are not interested in testing console statements.',
     )
+
+    test('returns a truthy value when calling methods on object with name included in options', () => {
+        const options: Partial<LogIgnorerOptions> = {
+            logignore: { objectNames: ['customLogger'] },
+        }
+        const logIgnorer = new LogIgnorer(options)
+        const path = parseExpressionStatement('customLogger.log("test")')
+        expect(logIgnorer.shouldIgnore(path)).toBeTruthy()
+    })
+
+    test('returns a falsy value when calling methods on other objects than console', () => {
+        const logIgnorer = new LogIgnorer()
+        const path = parseExpressionStatement('customLogger.log("test")')
+        expect(logIgnorer.shouldIgnore(path)).toBeFalsy()
+    })
+
+    test('returns a falsy value when calling methods on other objects than configured', () => {
+        const options: Partial<LogIgnorerOptions> = {
+            logignore: { objectNames: ['customLogger'] },
+        }
+        const logIgnorer = new LogIgnorer(options)
+        const path = parseExpressionStatement('foo.log("test")')
+        expect(logIgnorer.shouldIgnore(path)).toBeFalsy()
+    })
+
+    test('returns a falsy value when passing console as an argument to a method', () => {
+        const logIgnorer = new LogIgnorer()
+        const path = parseExpressionStatement('someObject.log(console)')
+        expect(logIgnorer.shouldIgnore(path)).toBeFalsy()
+    })
 })
+
+function parseExpressionStatement(
+    sourceCode: string,
+): NodePath {
+    const ast = parser.parse(sourceCode, { sourceType: 'module' })
+
+    let statementPath: NodePath | undefined
+    traverse(ast, {
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        ExpressionStatement(path) {
+            statementPath = path
+        },
+    })
+
+    if (!statementPath) {
+        throw new Error('Could not find expression statement in given code')
+    }
+
+    return statementPath
+}
