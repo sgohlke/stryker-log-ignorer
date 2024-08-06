f5b01e74c79216816a005b1fbaca8fddd224084e
sgohlke <gohlke.stefan@yahoo.de>
2024-08-06 11:10:13 +0200

    Ignore eslint issue

--- tests/NodePath.d.ts
+++ tests/NodePath.d.ts
@@ -2,5 +2,6 @@
 import type babel from '@babel/core'
 
 declare module '@stryker-mutator/api/ignore' {
+    // eslint-disable-next-line @typescript-eslint/no-empty-object-type
     export interface NodePath<T = babel.Node> extends babel.NodePath<T> {}
 }
