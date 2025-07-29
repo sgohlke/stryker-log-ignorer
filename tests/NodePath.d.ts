de1de8dfd57722df6f4667f62ead4428fdbb2f53
Stefan Gohlke <52039897+sgohlke@users.noreply.github.com>
2025-07-29 14:24:06 +0200

    Ignore linter findings

--- tests/NodePath.d.ts
+++ tests/NodePath.d.ts
@@ -2,6 +2,6 @@
 import type babel from '@babel/core'
 
 declare module '@stryker-mutator/api/ignore' {
-    // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+    // eslint-disable-next-line @typescript-eslint/no-empty-object-type @typescript-eslint/no-empty-interface id-length
     export interface NodePath<T = babel.Node> extends babel.NodePath<T> {}
 }
